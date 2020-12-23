b7e91978109f96137b84f56ba51d271341262f84
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-12-23 14:08:14 +0100

    [quality] Bypass tests for not finished code

--- develop.sh
+++ develop.sh
@@ -3,14 +3,14 @@ alias stop="docker kill ecmf"
 docker run --rm -d --name ecmf \
   -h ecmf \
   -v $PWD:/root \
+  -v $HOME/software:/software \
   -v $PWD:/etc/puppetlabs/code/modules/ecmf \
   -v $PWD/../easy_type:/etc/puppetlabs/code/modules/easy_type \
-  -v software:/software centos:7  /usr/sbin/init
-docker exec ecmf rpm -Uvh https://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
+  centos:7  /usr/sbin/init
+docker exec ecmf rpm -Uvh https://yum.puppet.com/puppet7/puppet-release-el-7.noarch.rpm
 docker exec ecmf rpm -Uvh https://yum.puppet.com/puppet-tools-release-el-7.noarch.rpm
-docker exec ecmf yum install puppet-agent puppet-bolt -y
-docker exec ecmf cp /software/Universal.entitlements /etc/puppetlabs/puppet/
-docker exec ecmf /opt/puppetlabs/puppet/bin/gem install byebug pry --no-ri --no-rdoc
-docker exec ecmf puppet module install puppetlabs-stdlib --force
-docker exec ecmf puppet module install ipcrm-echo --force
-docker exec -it ecmf bash
+docker exec ecmf yum install gcc make puppet-agent puppet-bolt -y
+docker exec ecmf /opt/puppetlabs/puppet/bin/gem install byebug pry --no-document
+docker exec ecmf /opt/puppetlabs/bin/puppet module install puppetlabs-stdlib --force
+docker exec ecmf /opt/puppetlabs/bin/puppet module install ipcrm-echo --force
+docker exec -it ecmf bash --login
